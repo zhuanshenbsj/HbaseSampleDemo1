944b826e6acb7bae386fe2fb3c2242c4527114b3
wcxu <wcxu@DESKTOP-8H194IH>
2019-03-04 19:26:42 +0800

    20190304-add phoneinfo demo

--- 2-cloudDay10/src/cn/itcast_01_hbase/HbaseTest.java
+++ 2-cloudDay10/src/cn/itcast_01_hbase/HbaseTest.java
@@ -1,10 +1,9 @@
 package cn.itcast_01_hbase;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hbase.Cell;
-import org.apache.hadoop.hbase.CellUtil;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.HTableDescriptor;
@@ -16,8 +15,6 @@ import org.apache.hadoop.hbase.client.ConnectionFactory;
 import org.apache.hadoop.hbase.client.Delete;
 import org.apache.hadoop.hbase.client.Get;
 import org.apache.hadoop.hbase.client.HBaseAdmin;
-import org.apache.hadoop.hbase.client.HConnection;
-import org.apache.hadoop.hbase.client.HConnectionManager;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.client.ResultScanner;
@@ -44,14 +41,30 @@ public class HbaseTest {
 	private Connection connection = null;
 	private Table table = null;
 
+	Random r = new Random();
+
 	@Before
 	public void init() throws Exception {
+		/**
+		 * HBaseConfiguration初始化会默认加载配置文件hbase-site.xml
+		 * @throws Exception
+		 */
 		config = HBaseConfiguration.create();// 配置
-		config.set("hbase.zookeeper.quorum", "name1,data1,data2,data3");// zookeeper地址
-		config.set("hbase.zookeeper.property.clientPort", "2181");// zookeeper端口
+		System.out.println(config.toString());
 		connection = ConnectionFactory.createConnection(config);
-		table = connection.getTable(TableName.valueOf("user"));
+		table = connection.getTable(TableName.valueOf("phone_info"));
 	}
+	// @Before
+	// public void init() throws Exception {
+	// config = HBaseConfiguration.create(new Configuration());// 配置
+	// config.set("hbase.zookeeper.quorum", "name1,data1,data2,data3");
+	// // zookeeper地址
+	// config.set("hbase.zookeeper.property.clientPort", "2181");
+	// // zookeeper端口
+	// System.out.println(config.toString());
+	// connection = ConnectionFactory.createConnection(config);
+	// table = connection.getTable(TableName.valueOf("phone_info"));
+	// }
 
 	/**
 	 * 创建一个表
@@ -61,29 +74,57 @@ public class HbaseTest {
 	@Test
 	public void createTable() throws Exception {
 		// 创建表管理类
-		HBaseAdmin admin = new HBaseAdmin(config); // hbase表管理
+		HBaseAdmin admin = (HBaseAdmin) connection.getAdmin(); // hbase表管理
 		// 创建表描述类
-		TableName tableName = TableName.valueOf("test3"); // 表名称
+		TableName tableName = TableName.valueOf("phone_info"); // 表名称
 		HTableDescriptor desc = new HTableDescriptor(tableName);
 		// 创建列族的描述类
 		HColumnDescriptor family = new HColumnDescriptor("info"); // 列族
+		family.setBlockCacheEnabled(true);
+		family.setInMemory(true);
+		family.setMaxVersions(1);
+		family.setTimeToLive(86400);
 		// 将列族添加到表中
 		desc.addFamily(family);
 		HColumnDescriptor family2 = new HColumnDescriptor("info2"); // 列族
 		// 将列族添加到表中
 		desc.addFamily(family2);
-		// 创建表
-		admin.createTable(desc); // 创建表
+
+		try {
+			// 创建表
+			if (!admin.tableExists(tableName)) {
+				admin.createTable(desc); // 创建表
+				System.out.println("#### table:" + tableName.toString() + " create successful!");
+			} else {
+				System.out.println("#### table:" + tableName.toString() + " already exists!");
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			admin.close();// 记得最后关闭链接
+		}
+
 	}
 
 	@Test
 	@SuppressWarnings("deprecation")
-	public void deleteTable() throws MasterNotRunningException,
-			ZooKeeperConnectionException, Exception {
-		HBaseAdmin admin = new HBaseAdmin(config);
-		admin.disableTable("test3");
-		admin.deleteTable("test3");
-		admin.close();
+	public void deleteTable() throws MasterNotRunningException, ZooKeeperConnectionException, Exception {
+		HBaseAdmin admin = (HBaseAdmin) connection.getAdmin();
+		TableName tableName = TableName.valueOf("phone_info");
+		try {
+			if (admin.tableExists(tableName)) {
+				admin.disableTable(tableName);// 先disable才能删除
+				admin.deleteTable(tableName);
+				System.out.println("#### table:" + tableName.toString() + " delete successful!");
+			} else {
+				System.out.println("table:" + tableName.toString() + " doesn't exists!");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			admin.close();
+		}
 	}
 
 	/**
@@ -96,18 +137,26 @@ public class HbaseTest {
 	public void insertData() throws Exception {
 		table.setAutoFlushTo(false);
 		table.setWriteBufferSize(534534534);
+		// rowkey: phonenumber_timestamp
 		ArrayList<Put> arrayList = new ArrayList<Put>();
-		for (int i = 21; i < 50; i++) {
-			Put put = new Put(Bytes.toBytes("1234"+i));
-			put.add(Bytes.toBytes("info1"), Bytes.toBytes("name"), Bytes.toBytes("wangwu"+i));
-			put.add(Bytes.toBytes("info1"), Bytes.toBytes("password"), Bytes.toBytes(1234+i));
+		for (int i = 20181201; i < 20181231; i++) {
+			Put put = new Put(Bytes.toBytes("18178656153_" + i));
+			put.add(Bytes.toBytes("info"), Bytes.toBytes("type"), Bytes.toBytes((i % 2 + "")));
+			put.add(Bytes.toBytes("info"), Bytes.toBytes("time"), Bytes.toBytes(i + ""));
+			put.add(Bytes.toBytes("info"), Bytes.toBytes("tophone"), Bytes.toBytes("17758651234"));
 			arrayList.add(put);
 		}
-		
-		//插入数据
-		table.put(arrayList);
-		//提交
-		table.flushCommits();
+
+		try {
+			// 插入数据
+			table.put(arrayList);
+			// 提交
+			table.flushCommits();
+			System.out.println("#### table:" + table.getName() + " insert successful!");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	/**
@@ -116,43 +165,42 @@ public class HbaseTest {
 	 * @throws Exception
 	 */
 	@Test
-	public void uodateData() throws Exception {
-		Put put = new Put(Bytes.toBytes("1234"));
-		put.add(Bytes.toBytes("info1"), Bytes.toBytes("namessss"), Bytes.toBytes("lisi1234"));
-		put.add(Bytes.toBytes("info1"), Bytes.toBytes("password"), Bytes.toBytes(1234));
-		//插入数据
+	public void updateData() throws Exception {
+		Put put = new Put(Bytes.toBytes("18178656153_20181201"));
+		put.add(Bytes.toBytes("info"), Bytes.toBytes("tophone"), Bytes.toBytes("17758651235"));
+		// 插入数据
 		table.put(put);
-		//提交
+		// 提交
 		table.flushCommits();
 	}
 
 	/**
-	 * 删除数据:删除rowkey
+	 * 删除数据:通过rowkey删除一整行数据
 	 * 
 	 * @throws Exception
 	 */
 	@Test
 	public void deleteRowKey() throws Exception {
-		Delete delete = new Delete(Bytes.toBytes("1234"));
+		Delete delete = new Delete(Bytes.toBytes("18178656153_20181201"));
 		table.delete(delete);
 		table.flushCommits();
 	}
-	
+
 	/**
-	 * 删除数据:删除列族
+	 * 删除数据:通过rowkey和列族删除指定行的某一列族所有数据
 	 * 
 	 * @throws Exception
 	 */
 	@Test
 	public void deleteColumnFamily() throws Exception {
-		Delete delete = new Delete(Bytes.toBytes("1234"));
+		Delete delete = new Delete(Bytes.toBytes("18178656153_20181201"));
 		delete.addFamily(Bytes.toBytes("info2"));
 		table.delete(delete);
 		table.flushCommits();
 	}
-	
+
 	/**
-	 * 删除数据:删除列
+	 * 删除数据:通过rowkey和列族和两名删除指定行的某一列族指定列数据
 	 * 
 	 * @throws Exception
 	 */
@@ -171,13 +219,16 @@ public class HbaseTest {
 	 */
 	@Test
 	public void queryData() throws Exception {
-		//也可以查单个列族或者单个列,get.add 类似delete
-		Get get = new Get(Bytes.toBytes("1111"));
-		//get.addColumn(Bytes.toBytes("info1"), Bytes.toBytes("age"));
+		// 如果没有addFamily/addColumn就是查询所有列
+		Get get = new Get(Bytes.toBytes("18178656153_20181201"));
+		// get.addFamily(Bytes.toBytes("info"));
+		// get.addColumn(Bytes.toBytes("info"), Bytes.toBytes("type"));
+		// get.addColumn(Bytes.toBytes("info"), Bytes.toBytes("time"));
+		// get.addColumn(Bytes.toBytes("info"), Bytes.toBytes("tophone"));
 		Result result = table.get(get);
-		System.out.println(Bytes.toInt(result.getValue(Bytes.toBytes("info1"), Bytes.toBytes("password"))));
-		System.out.println(Bytes.toString(result.getValue(Bytes.toBytes("info1"), Bytes.toBytes("namessss"))));
-		System.out.println(Bytes.toString(result.getValue(Bytes.toBytes("info1"), Bytes.toBytes("age"))));
+		System.out.println(Bytes.toString(result.getValue(Bytes.toBytes("info"), Bytes.toBytes("type"))));
+		System.out.println(Bytes.toString(result.getValue(Bytes.toBytes("info"), Bytes.toBytes("time"))));
+		System.out.println(Bytes.toString(result.getValue(Bytes.toBytes("info"), Bytes.toBytes("tophone"))));
 	}
 
 	/**
@@ -188,22 +239,23 @@ public class HbaseTest {
 	@Test
 	public void scanData() throws Exception {
 		Scan scan = new Scan();
-		//scan.addFamily(Bytes.toBytes("info"));
-		//scan.addColumn(Bytes.toBytes("info"), Bytes.toBytes("password"));
+		// scan.addFamily(Bytes.toBytes("info"));
+		// scan.addColumn(Bytes.toBytes("info"), Bytes.toBytes("password"));
 		scan.setStartRow(Bytes.toBytes("12341"));
 		scan.setStopRow(Bytes.toBytes("12345"));
 		ResultScanner scanner = table.getScanner(scan);
 		for (Result result : scanner) {
 			System.out.println(Bytes.toInt(result.getValue(Bytes.toBytes("info1"), Bytes.toBytes("password"))));
 			System.out.println(Bytes.toString(result.getValue(Bytes.toBytes("info1"), Bytes.toBytes("name"))));
-			//System.out.println(Bytes.toInt(result.getValue(Bytes.toBytes("info2"), Bytes.toBytes("password"))));
-			//System.out.println(Bytes.toString(result.getValue(Bytes.toBytes("info2"), Bytes.toBytes("name"))));
+			// System.out.println(Bytes.toInt(result.getValue(Bytes.toBytes("info2"),
+			// Bytes.toBytes("password"))));
+			// System.out.println(Bytes.toString(result.getValue(Bytes.toBytes("info2"),
+			// Bytes.toBytes("name"))));
 		}
 	}
 
 	/**
-	 * 全表扫描的过滤器
-	 * 列值过滤器
+	 * 全表扫描的过滤器 :列值过滤器
 	 * 
 	 * @throws Exception
 	 */
@@ -212,10 +264,9 @@ public class HbaseTest {
 
 		// 创建全表扫描的scan
 		Scan scan = new Scan();
-		//过滤器：列值过滤器
-		SingleColumnValueFilter filter = new SingleColumnValueFilter(Bytes.toBytes("info"),
-				Bytes.toBytes("name"), CompareFilter.CompareOp.EQUAL,
-				Bytes.toBytes("zhangsan2"));
+		// 过滤器：列值过滤器
+		SingleColumnValueFilter filter = new SingleColumnValueFilter(Bytes.toBytes("info"), Bytes.toBytes("name"),
+				CompareFilter.CompareOp.EQUAL, Bytes.toBytes("zhangsan2"));
 		// 设置过滤器
 		scan.setFilter(filter);
 
@@ -224,21 +275,25 @@ public class HbaseTest {
 		for (Result result : scanner) {
 			System.out.println(Bytes.toInt(result.getValue(Bytes.toBytes("info"), Bytes.toBytes("password"))));
 			System.out.println(Bytes.toString(result.getValue(Bytes.toBytes("info"), Bytes.toBytes("name"))));
-			//System.out.println(Bytes.toInt(result.getValue(Bytes.toBytes("info2"), Bytes.toBytes("password"))));
-			//System.out.println(Bytes.toString(result.getValue(Bytes.toBytes("info2"), Bytes.toBytes("name"))));
+			// System.out.println(Bytes.toInt(result.getValue(Bytes.toBytes("info2"),
+			// Bytes.toBytes("password"))));
+			// System.out.println(Bytes.toString(result.getValue(Bytes.toBytes("info2"),
+			// Bytes.toBytes("name"))));
 		}
 
 	}
+
 	/**
-	 * rowkey过滤器
+	 * 全表扫描的过滤器 :rowkey过滤器
+	 * 
 	 * @throws Exception
 	 */
 	@Test
 	public void scanDataByFilter2() throws Exception {
-		
+
 		// 创建全表扫描的scan
 		Scan scan = new Scan();
-		//匹配rowkey以wangsenfeng开头的
+		// 匹配rowkey以wangsenfeng开头的
 		RowFilter filter = new RowFilter(CompareFilter.CompareOp.EQUAL, new RegexStringComparator("^12341"));
 		// 设置过滤器
 		scan.setFilter(filter);
@@ -247,23 +302,25 @@ public class HbaseTest {
 		for (Result result : scanner) {
 			System.out.println(Bytes.toInt(result.getValue(Bytes.toBytes("info"), Bytes.toBytes("password"))));
 			System.out.println(Bytes.toString(result.getValue(Bytes.toBytes("info"), Bytes.toBytes("name"))));
-			//System.out.println(Bytes.toInt(result.getValue(Bytes.toBytes("info2"), Bytes.toBytes("password"))));
-			//System.out.println(Bytes.toString(result.getValue(Bytes.toBytes("info2"), Bytes.toBytes("name"))));
+			// System.out.println(Bytes.toInt(result.getValue(Bytes.toBytes("info2"),
+			// Bytes.toBytes("password"))));
+			// System.out.println(Bytes.toString(result.getValue(Bytes.toBytes("info2"),
+			// Bytes.toBytes("name"))));
 		}
 
-		
 	}
-	
+
 	/**
-	 * 匹配列名前缀
+	 * 全表扫描的过滤器 :匹配列名前缀过滤器
+	 * 
 	 * @throws Exception
 	 */
 	@Test
 	public void scanDataByFilter3() throws Exception {
-		
+
 		// 创建全表扫描的scan
 		Scan scan = new Scan();
-		//匹配rowkey以wangsenfeng开头的
+		// 匹配rowkey以wangsenfeng开头的
 		ColumnPrefixFilter filter = new ColumnPrefixFilter(Bytes.toBytes("na"));
 		// 设置过滤器
 		scan.setFilter(filter);
@@ -271,61 +328,54 @@ public class HbaseTest {
 		ResultScanner scanner = table.getScanner(scan);
 		for (Result result : scanner) {
 			System.out.println("rowkey：" + Bytes.toString(result.getRow()));
-			System.out.println("info:name："
-					+ Bytes.toString(result.getValue(Bytes.toBytes("info"),
-							Bytes.toBytes("name"))));
+			System.out.println(
+					"info:name：" + Bytes.toString(result.getValue(Bytes.toBytes("info"), Bytes.toBytes("name"))));
 			// 判断取出来的值是否为空
 			if (result.getValue(Bytes.toBytes("info"), Bytes.toBytes("age")) != null) {
-				System.out.println("info:age："
-						+ Bytes.toInt(result.getValue(Bytes.toBytes("info"),
-								Bytes.toBytes("age"))));
+				System.out.println(
+						"info:age：" + Bytes.toInt(result.getValue(Bytes.toBytes("info"), Bytes.toBytes("age"))));
 			}
 			// 判断取出来的值是否为空
 			if (result.getValue(Bytes.toBytes("info"), Bytes.toBytes("sex")) != null) {
-				System.out.println("infi:sex："
-						+ Bytes.toInt(result.getValue(Bytes.toBytes("info"),
-								Bytes.toBytes("sex"))));
+				System.out.println(
+						"infi:sex：" + Bytes.toInt(result.getValue(Bytes.toBytes("info"), Bytes.toBytes("sex"))));
 			}
 			// 判断取出来的值是否为空
 			if (result.getValue(Bytes.toBytes("info2"), Bytes.toBytes("name")) != null) {
-				System.out
-				.println("info2:name："
-						+ Bytes.toString(result.getValue(
-								Bytes.toBytes("info2"),
-								Bytes.toBytes("name"))));
+				System.out.println(
+						"info2:name：" + Bytes.toString(result.getValue(Bytes.toBytes("info2"), Bytes.toBytes("name"))));
 			}
 			// 判断取出来的值是否为空
 			if (result.getValue(Bytes.toBytes("info2"), Bytes.toBytes("age")) != null) {
-				System.out.println("info2:age："
-						+ Bytes.toInt(result.getValue(Bytes.toBytes("info2"),
-								Bytes.toBytes("age"))));
+				System.out.println(
+						"info2:age：" + Bytes.toInt(result.getValue(Bytes.toBytes("info2"), Bytes.toBytes("age"))));
 			}
 			// 判断取出来的值是否为空
 			if (result.getValue(Bytes.toBytes("info2"), Bytes.toBytes("sex")) != null) {
-				System.out.println("info2:sex："
-						+ Bytes.toInt(result.getValue(Bytes.toBytes("info2"),
-								Bytes.toBytes("sex"))));
+				System.out.println(
+						"info2:sex：" + Bytes.toInt(result.getValue(Bytes.toBytes("info2"), Bytes.toBytes("sex"))));
 			}
 		}
-		
+
 	}
+
 	/**
-	 * 过滤器集合
+	 * 全表扫描的过滤器 :过滤器集合
+	 * 
 	 * @throws Exception
 	 */
 	@Test
 	public void scanDataByFilter4() throws Exception {
-		
+
 		// 创建全表扫描的scan
 		Scan scan = new Scan();
-		//过滤器集合：MUST_PASS_ALL（and）,MUST_PASS_ONE(or)
+		// 过滤器集合：MUST_PASS_ALL（and）,MUST_PASS_ONE(or)
 		FilterList filterList = new FilterList(Operator.MUST_PASS_ONE);
-		//匹配rowkey以wangsenfeng开头的
+		// 匹配rowkey以wangsenfeng开头的
 		RowFilter filter = new RowFilter(CompareFilter.CompareOp.EQUAL, new RegexStringComparator("^wangsenfeng"));
-		//匹配name的值等于wangsenfeng
-		SingleColumnValueFilter filter2 = new SingleColumnValueFilter(Bytes.toBytes("info"),
-				Bytes.toBytes("name"), CompareFilter.CompareOp.EQUAL,
-				Bytes.toBytes("zhangsan"));
+		// 匹配name的值等于wangsenfeng
+		SingleColumnValueFilter filter2 = new SingleColumnValueFilter(Bytes.toBytes("info"), Bytes.toBytes("name"),
+				CompareFilter.CompareOp.EQUAL, Bytes.toBytes("zhangsan"));
 		filterList.addFilter(filter);
 		filterList.addFilter(filter2);
 		// 设置过滤器
@@ -334,43 +384,35 @@ public class HbaseTest {
 		ResultScanner scanner = table.getScanner(scan);
 		for (Result result : scanner) {
 			System.out.println("rowkey：" + Bytes.toString(result.getRow()));
-			System.out.println("info:name："
-					+ Bytes.toString(result.getValue(Bytes.toBytes("info"),
-							Bytes.toBytes("name"))));
+			System.out.println(
+					"info:name：" + Bytes.toString(result.getValue(Bytes.toBytes("info"), Bytes.toBytes("name"))));
 			// 判断取出来的值是否为空
 			if (result.getValue(Bytes.toBytes("info"), Bytes.toBytes("age")) != null) {
-				System.out.println("info:age："
-						+ Bytes.toInt(result.getValue(Bytes.toBytes("info"),
-								Bytes.toBytes("age"))));
+				System.out.println(
+						"info:age：" + Bytes.toInt(result.getValue(Bytes.toBytes("info"), Bytes.toBytes("age"))));
 			}
 			// 判断取出来的值是否为空
 			if (result.getValue(Bytes.toBytes("info"), Bytes.toBytes("sex")) != null) {
-				System.out.println("infi:sex："
-						+ Bytes.toInt(result.getValue(Bytes.toBytes("info"),
-								Bytes.toBytes("sex"))));
+				System.out.println(
+						"infi:sex：" + Bytes.toInt(result.getValue(Bytes.toBytes("info"), Bytes.toBytes("sex"))));
 			}
 			// 判断取出来的值是否为空
 			if (result.getValue(Bytes.toBytes("info2"), Bytes.toBytes("name")) != null) {
-				System.out
-				.println("info2:name："
-						+ Bytes.toString(result.getValue(
-								Bytes.toBytes("info2"),
-								Bytes.toBytes("name"))));
+				System.out.println(
+						"info2:name：" + Bytes.toString(result.getValue(Bytes.toBytes("info2"), Bytes.toBytes("name"))));
 			}
 			// 判断取出来的值是否为空
 			if (result.getValue(Bytes.toBytes("info2"), Bytes.toBytes("age")) != null) {
-				System.out.println("info2:age："
-						+ Bytes.toInt(result.getValue(Bytes.toBytes("info2"),
-								Bytes.toBytes("age"))));
+				System.out.println(
+						"info2:age：" + Bytes.toInt(result.getValue(Bytes.toBytes("info2"), Bytes.toBytes("age"))));
 			}
 			// 判断取出来的值是否为空
 			if (result.getValue(Bytes.toBytes("info2"), Bytes.toBytes("sex")) != null) {
-				System.out.println("info2:sex："
-						+ Bytes.toInt(result.getValue(Bytes.toBytes("info2"),
-								Bytes.toBytes("sex"))));
+				System.out.println(
+						"info2:sex：" + Bytes.toInt(result.getValue(Bytes.toBytes("info2"), Bytes.toBytes("sex"))));
 			}
 		}
-		
+
 	}
 
 	@After
